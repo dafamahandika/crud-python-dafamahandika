b910b07ab3a016ce2d0a05867543795d981afdf9
dafamahandika <dafamahandika@gmail.com>
2022-11-10 16:54:31 +0700

    tambah main

---
+++ main.py
@@ -0,0 +1,46 @@
+# x = (input())
+# y = (input())
+
+# print (x + y)
+
+# x = 1 // 2 * 3
+# print(x)
+
+# x = 1
+# y = 2
+# z = x
+# x = y
+# y = z 
+# print(x ,y)
+
+# z = y = x = 1
+# print(x , y, z, sep='*')
+
+# x = [1, 2, 3, 4]
+# print(x[-3:-2])
+
+# var = 0
+# while var < 6:
+#      var += 1
+#      if var % 2 == 0:
+#           continue
+#      print("#") 
+
+
+# vals = [1, 2, 3]
+# vals = nums[-1:-2]
+# print(vals)
+
+
+# print(vals)
+
+# t = [[3 - i for i in range (3)] for j in range(3)]
+# s = 0
+# for i in range(3):
+#      s += t[i][i]
+# print(s)
+
+# z = 10
+# y = 0
+# x = y < z and z > y or y >z and z < y
+# print(x)
